26437966c062b73624cc1614825d11ea24f052c1
Lukasz <lukzerom@gmail.com>
2020-02-08 18:40:42 +0100

    First component - Search.jsx - chaget using useState, more changes coming soon

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,46 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Useritem from './Useritem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        login: 'mojombo',
-        id: 1,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        login: 'defunkt',
-        id: 2,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      },
-      {
-        login: 'pjhyett',
-        id: 3,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett'
-      },
-      {
-        login: 'wycats',
-        id: 4,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/4?v=4',
-        html_url: 'https://github.com/wycats'
-      }
-    ]
-  };
-
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {users.map(user => (
           <Useritem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 
 const userStyle = {
   display: 'grid',
